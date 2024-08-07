1494fbf653d3b4c2554b249926bbc3621de2965e
livecloud-labs <livecloud.store@gmail.com>
2024-08-07 14:24:47 +0100

    chore: minor lgic update

--- src/test/lib/channel-manager.test.ts
+++ src/test/lib/channel-manager.test.ts
@@ -3,11 +3,20 @@ import { describe, expect, it } from 'vitest';
 
 describe('ChannelManager', () => {
   describe('getRoutingKey', () => {
-    it('should return the correct routing key for a given namespace', () => {
+    it('should return the correct routing key for a given namespace with suffix', () => {
       const routingKey = ChannelManager.getRoutingKey('appPid:nsp:123:456');
       const [routingKeyprefix, appPid, hashedNamespace] = routingKey.split(':');
 
       expect(hashedNamespace).toHaveLength(2);
+      expect(appPid).toBe('appPid');
+      expect(routingKeyprefix).toBe('$$');
+    });
+
+    it('should return the correct routing key for a given namespace without suffix', () => {
+      const routingKey = ChannelManager.getRoutingKey('appPid:nsp');
+      const [routingKeyprefix, appPid, hashedNamespace] = routingKey.split(':');
+
+      expect(hashedNamespace).toHaveLength(1);
       expect(appPid).toBe('appPid');
       expect(routingKeyprefix).toBe('$$');
     });

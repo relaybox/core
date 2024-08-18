9d69604147a552629d58ae080baf590db2b8cfa1
livecloud-labs <livecloud.store@gmail.com>
2024-08-18 14:36:27 +0100

    chore: update ws fetch validation

--- src/modules/history/history.handlers.ts
+++ src/modules/history/history.handlers.ts
@@ -8,6 +8,7 @@ import { formatErrorResponse } from '../../util/format';
 import { permissionsGuard } from '../guards/guards.service';
 import { DsPermission } from '../../types/permissions.types';
 import { extractRoomId } from '../../util/helpers';
+import { HistoryOrder } from '../../types/history.types';
 
 export async function clientRoomHistoryGet(
   logger: Logger,
@@ -17,20 +18,44 @@ export async function clientRoomHistoryGet(
   res: SocketAckHandler
 ): Promise<void> {
   const session = socket.getUserData();
-  const { nspRoomId, start, end, seconds, limit, nextPageToken, items, order } = data;
+  let { nspRoomId, start, end, seconds, limit, nextPageToken, items, order } = data;
   const roomId = extractRoomId(nspRoomId);
 
   try {
     permissionsGuard(roomId, DsPermission.HISTORY, session.permissions);
+
+    start = start || null;
+    end = end || null;
+    seconds = seconds || HISTORY_MAX_SECONDS;
+    limit = limit || HISTORY_MAX_LIMIT;
+    items = items || null;
+    order = order || HistoryOrder.DESC;
+    nextPageToken = nextPageToken || null;
+
+    if (seconds && seconds > HISTORY_MAX_SECONDS) {
+      throw new Error(`Invalid seconds parameter`);
+    }
+
+    if (limit > HISTORY_MAX_LIMIT) {
+      throw new Error(`Invalid limit parameter`);
+    }
+
+    if (order !== HistoryOrder.DESC && !seconds && !start) {
+      throw new Error(
+        `Either 'seconds' or 'start' must be provided when order is ${HistoryOrder.ASC}`
+      );
+    }
+
+    console.log(start, end, seconds, limit, items, order, nextPageToken);
 
     const historyData = await getRoomHistoryMessages(
       redisClient,
       nspRoomId,
       start,
       end,
-      seconds || HISTORY_MAX_SECONDS,
-      limit || HISTORY_MAX_LIMIT,
-      items || null,
+      seconds,
+      limit,
+      items,
       order,
       nextPageToken
     );

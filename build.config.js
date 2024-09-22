a54ef121db9e2ea9dba7dda4448509f209bce63f
livecloud-labs <livecloud.store@gmail.com>
2024-09-22 22:14:49 +0100

    chore: refine build

--- build.config.js
+++ build.config.js
@@ -1,4 +1,7 @@
 const esbuild = require('esbuild');
+const path = require('path');
+const fs = require('fs');
+const pkg = require('./package.json');
 
 esbuild
   .build({
@@ -7,8 +10,26 @@ esbuild
     platform: 'node',
     target: 'node20',
     outfile: 'build/server.js',
-    external: ['uWebSockets.js', './*.node'],
+    external: [
+      'uWebSockets.js',
+      './*.node',
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
     plugins: [],
     minify: true
+  })
+  .then(() => {
+    const srcCertsDir = path.resolve(__dirname, 'src', 'certs');
+    const destCertsDir = path.resolve(__dirname, 'build', 'certs');
+
+    try {
+      // Copy the certs directory recursively
+      fs.cpSync(srcCertsDir, destCertsDir, { recursive: true });
+      console.log('Certs directory copied successfully.');
+    } catch (err) {
+      console.error('Error copying certs directory:', err);
+      process.exit(1);
+    }
   })
   .catch(() => process.exit(1));
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -19,11 +19,19 @@ const REDIS_TLS_DISABLED = process.env.REDIS_TLS_DISABLED === 'true';
 
 export type RedisClient = RedisClientType<RedisModules, RedisFunctions, RedisScripts>;
 
+function getCeritificate(): Buffer | undefined {
+  try {
+    return fs.readFileSync(path.join(__dirname, 'certs/AmazonRootCA1.pem'));
+  } catch (err) {
+    logger.error(`Failed to read certificate file: ${err}`);
+  }
+}
+
 // Node redis client options
 export const tlsConnectionOptions = {
   tls: true,
   rejectUnauthorized: true,
-  cert: 'hello' //fs.readFileSync(path.join(__dirname, '../certs/AmazonRootCA1.pem'))
+  cert: getCeritificate()
 };
 
 export const socketOptions = {

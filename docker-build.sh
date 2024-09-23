edd5cae824dd0cc8b2e16d9b4b5cd99712391b0c
livecloud-labs <livecloud.store@gmail.com>
2024-09-23 09:06:41 +0100

    chore: remove compose and release scripts

--- docker-build.sh
+++ docker-build.sh
@@ -3,7 +3,3 @@ VERSION=$(cat package.json | grep '"version":' | awk -F'"' '{print $4}')
 
 # Tag the image with both 'latest' and the specific version number
 docker build -t relaybox/uws:latest -t relaybox/uws:$VERSION .
-
-# Push both the 'latest' and versioned tags to Docker Hub
-# docker push relaybox/uws:latest
-# docker push relaybox/uws:$VERSION

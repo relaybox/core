4ebd01e26e8799854e4ffef4b09ba90093e52e5a
livecloud-labs <livecloud.store@gmail.com>
2024-09-28 15:52:41 +0100

    chore: extend event publish

--- src/modules/event/event.handlers.ts
+++ src/modules/event/event.handlers.ts
@@ -1,6 +1,7 @@
 import { getJsonResponse } from '@/util/http';
 import { getLogger } from '@/util/logger';
 import { HttpRequest, HttpResponse } from 'uWebSockets.js';
+import { v4 as uuid } from 'uuid';
 
 const logger = getLogger('event');
 
@@ -23,12 +24,14 @@ const parseBody = (res: HttpResponse) => {
 export async function clientEventPublish(res: HttpResponse, req: HttpRequest): Promise<void> {
   let aborted = false;
 
-  logger.info('Publishing event');
-
   try {
     res.onAborted(() => {
       aborted = true;
     });
+
+    const requestId = uuid();
+
+    logger.info('Publishing event', { requestId });
 
     const publicKey = req.getHeader(`X-Ds-Public-Key`) || req.getHeader(`x-ds-public-key`);
     const signature = req.getHeader(`X-Ds-Req-Signature`) || req.getHeader(`x-ds-req-signature`);
@@ -38,12 +41,24 @@ export async function clientEventPublish(res: HttpResponse, req: HttpRequest): P
     }
 
     const body = await parseBody(res);
+    const timestamp = new Date().toISOString();
 
-    console.log(body);
+    const jobData = {
+      publicKey,
+      signature,
+      requestId,
+      timestamp,
+      body
+    };
 
     if (!aborted) {
       res.cork(() => {
-        getJsonResponse(res, '200 ok').end(JSON.stringify({ status: 200 }));
+        getJsonResponse(res, '200 ok').end(
+          JSON.stringify({
+            requestId,
+            timestamp
+          })
+        );
       });
     }
   } catch (err: any) {

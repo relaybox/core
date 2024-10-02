ad25db176864737ed368043dc4858e44e3fda0fe
livecloud-labs <livecloud.store@gmail.com>
2024-10-02 09:34:55 +0100

    chore: rename test dir for consistency

--- tests/__mocks__/external/uWebsockets.ts
+++
@@ -1,7 +0,0 @@
-import { TemplatedApp } from 'uWebSockets.js';
-import { vi } from 'vitest';
-
-export const mockApp = {
-  publish: vi.fn(),
-  numSubscribers: vi.fn().mockReturnValue(1)
-} as unknown as TemplatedApp;

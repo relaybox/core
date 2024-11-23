dd2cbdce4ab5f3e969ef6c9ac6a5a7420b1714da
livecloud-labs <livecloud.store@gmail.com>
2024-11-23 14:44:59 +0000

    chore: update logger [skip ci]

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,18 +1,18 @@
-import winston from 'winston';
-import { colorize } from 'json-colorizer';
+import winston, { format } from 'winston';
 
-enum LogLevel {
+const { printf, combine, timestamp, errors, colorize: winstonColorize } = format;
+
+export enum LogLevel {
   LOG = 'log',
   INFO = 'info',
   WARN = 'warn',
   DEBUG = 'debug'
 }
 
-const easyLogFormat = winston.format.printf((info) => {
+const easyLogFormat = printf((info) => {
   const { level, service, message } = info;
 
   let baseLog = `[${level}]:${new Date().toISOString().slice(11)} ${service} - ${message}`;
-  // .slice(11)} ${service} - ${message}\n${colorize(JSON.stringify(info, null, 2))}\n`;
 
   if (info.err && info.err.stack) {
     baseLog += `\n${info.err.stack}`;
@@ -21,39 +21,14 @@ const easyLogFormat = winston.format.printf((info) => {
   return baseLog;
 });
 
-const customLogFormat = winston.format.printf((info) => {
-  const { level, ...rest } = info;
-
-  const logDetails = JSON.stringify(rest, null, 4);
-
-  return `[${level}]: ${logDetails}`;
-});
-
-const prettyPrint = new winston.transports.Console({
-  level: process.env.LOG_LEVEL || LogLevel.INFO,
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.colorize(),
-    customLogFormat
-  )
-});
-
 const easyPrint = new winston.transports.Console({
   level: process.env.LOG_LEVEL || LogLevel.INFO,
-  format: winston.format.combine(
-    winston.format.errors({ stack: true }),
-    winston.format.timestamp(),
-    winston.format.colorize(),
-    easyLogFormat
-  )
+  format: combine(errors({ stack: true }), timestamp(), winstonColorize(), easyLogFormat)
 });
 
 const flatPrint = new winston.transports.Console({
   level: process.env.LOG_LEVEL || LogLevel.INFO,
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.errors({ stack: true }) // Ensures stack trace is captured
-  )
+  format: combine(timestamp(), errors({ stack: true }))
 });
 
 const transports = process.env.LOCALHOST === 'true' ? [easyPrint] : [flatPrint];
@@ -62,8 +37,6 @@ const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || LogLevel.INFO,
   transports
 });
-
-// logger.transports[0].silent = true;
 
 export function getLogger(service: string) {
   return logger.child({ service });
